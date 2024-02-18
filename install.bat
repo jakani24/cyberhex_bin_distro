db20c75dc06a89bc84d74055b435402f7e66a3e8
Janis Steiner <89935073+jakani24@users.noreply.github.com>
2024-02-18 11:32:10 +0000

    Add files via upload

--- install.bat
+++ install.bat
@@ -1,12 +1,14 @@
 #cyberhex installer downloader
 @echo off
 echo "Downloading Cyberhex installer"
-curl -O https://github.com/jakani24/cyberhex_bin_distro/raw/main/ma_installer.exe
+curl -O -L https://github.com/jakani24/cyberhex_bin_distro/raw/main/ma_installer.exe
 echo "Downloading dll files"
 curl -O -L https://github.com/jakani24/cyberhex_bin_distro/raw/main/libcrypto-3-x64.dll
 curl -O -L https://github.com/jakani24/cyberhex_bin_distro/raw/main/libcurl.dll
 curl -O -L https://github.com/jakani24/cyberhex_bin_distro/raw/main/zlib1.dll
 curl -O -L https://github.com/jakani24/cyberhex_bin_distro/raw/main/client_frontend.exe
+curl -O -L https://github.com/jakani24/cyberhex_bin_distro/raw/main/client_backend.exe
+curl -O -L https://github.com/jakani24/cyberhex_bin_distro/raw/main/ma_uninstaller.exe
 curl -O -L https://github.com/jakani24/cyberhex_bin_distro/raw/main/libcurl-d.dll
 curl -O -L https://github.com/jakani24/cyberhex_bin_distro/raw/main/msvcp140.dll
 curl -O -L https://github.com/jakani24/cyberhex_bin_distro/raw/main/vcruntime140.dll
